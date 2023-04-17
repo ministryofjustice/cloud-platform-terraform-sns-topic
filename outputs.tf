1111ba680be76b341c5f9b566457ff9a8cd2185f
sj-williams <steve.williams@digital.justice.gov.uk>
2023-04-17 15:19:23 +0100

    test multi iam user & keys for topic

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,6 @@
 output "user_name" {
   description = "IAM user with access to the topic"
-  value       = aws_iam_user.user.name
+  value       = aws_iam_user.user[var.team_name].name
 }
 
 output "topic_name" {
@@ -15,10 +15,18 @@ output "topic_arn" {
 
 output "access_key_id" {
   description = "The access key ID"
-  value       = aws_iam_access_key.user.id
+  value       = aws_iam_access_key.user[var.team_name].id
 }
 
 output "secret_access_key" {
   description = "The secret access key ID"
-  value       = aws_iam_access_key.user.secret
+  value       = aws_iam_access_key.user[var.team_name].secret
+}
+
+output "access_keys" {
+  description = "The list of access keys for all teams"
+  value = [for key in aws_iam_access_key.user : {
+    id = key.id,
+    secret = key.secret
+  }]
 }
