e1d3d632319d09b0957947fb6669bd59bc5ff28f
Matt Whittaker <34448412+mattops@users.noreply.github.com>
2023-04-25 11:50:22 +0100

    fix name

--- outputs.tf
+++ outputs.tf
@@ -34,7 +34,7 @@ output "additional_access_keys" {
   }
 }
 
-output "aws_iam_policy" {
-  description = "The iam policy with permissions for this SNS topic, use this policy to create IRSA based kubernetes service accounts."
+output "aws_iam_policy_arn" {
+  description = "The arn of the policy defining permissions for this SNS topic, use this policy to create IRSA based kubernetes service accounts."
   value       = try(aws_iam_policy.irsa.arn, "")
 }
