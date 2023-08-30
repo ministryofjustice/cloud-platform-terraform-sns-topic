1c2dda32eb6ba9e958a065a9cf708ea4f7c3c36c
jakemulley <jakemulley@users.noreply.github.com>
2023-08-30 14:33:22 +0100

    Remove access keys

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 locals {
   # Generic configuration
-  identifier         = "cloud-platform-${var.team_name}-${random_id.id.hex}"
-  additional_clients = toset(var.additional_topic_clients)
+  identifier = "cloud-platform-${var.team_name}-${random_id.id.hex}"
 
   # Tags
   default_tags = {
@@ -114,82 +113,6 @@ resource "aws_sns_topic" "new_topic" {
   kms_master_key_id = var.encrypt_sns_kms ? aws_kms_key.kms[0].arn : null
 
   tags = local.default_tags
-}
-
-# Legacy long-lived credentials
-resource "aws_iam_user" "user" {
-  name = "cp-sns-topic-${random_id.id.hex}"
-  path = "/system/sns-topic-user/${var.team_name}/"
-}
-
-resource "aws_iam_access_key" "user" {
-  user = aws_iam_user.user.name
-}
-
-data "aws_iam_policy_document" "policy" {
-  statement {
-    actions = [
-      "sns:ListEndpointsByPlatformApplication",
-      "sns:ListPlatformApplications",
-      "sns:ListSubscriptions",
-      "sns:ListSubscriptionsByTopic",
-      "sns:ListTopics",
-      "sns:CheckIfPhoneNumberIsOptedOut",
-      "sns:GetEndpointAttributes",
-      "sns:GetPlatformApplicationAttributes",
-      "sns:GetSMSAttributes",
-      "sns:GetSubscriptionAttributes",
-      "sns:GetTopicAttributes",
-      "sns:ListPhoneNumbersOptedOut",
-      "sns:ConfirmSubscription",
-      "sns:CreatePlatformApplication",
-      "sns:CreatePlatformEndpoint",
-      "sns:DeleteEndpoint",
-      "sns:DeletePlatformApplication",
-      "sns:OptInPhoneNumber",
-      "sns:Publish",
-      "sns:SetEndpointAttributes",
-      "sns:SetPlatformApplicationAttributes",
-      "sns:SetSMSAttributes",
-      "sns:SetSubscriptionAttributes",
-      "sns:SetTopicAttributes",
-      "sns:Subscribe",
-      "sns:Unsubscribe",
-    ]
-
-    resources = [
-      aws_sns_topic.new_topic.arn,
-    ]
-  }
-}
-
-resource "aws_iam_user_policy" "policy" {
-  name   = "sns-topic"
-  policy = data.aws_iam_policy_document.policy.json
-  user   = aws_iam_user.user.name
-}
-
-resource "random_id" "additional_user_id" {
-  for_each    = local.additional_clients
-  byte_length = 16
-}
-
-resource "aws_iam_user" "additional_users" {
-  for_each = local.additional_clients
-  name     = "cp-sns-topic-${random_id.additional_user_id[each.value].hex}"
-  path     = "/system/sns-topic-user/${each.value}/"
-}
-
-resource "aws_iam_access_key" "additional_users" {
-  for_each = local.additional_clients
-  user     = aws_iam_user.additional_users[each.value].name
-}
-
-resource "aws_iam_user_policy" "additional_users_policy" {
-  for_each = local.additional_clients
-  name     = "sns-topic"
-  policy   = data.aws_iam_policy_document.policy.json
-  user     = aws_iam_user.additional_users[each.value].name
 }
 
 ##############################
--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "user_name" {
-  description = "IAM user with access to the topic"
-  value       = aws_iam_user.user.name
-}
-
 output "topic_name" {
   description = "ARN for the topic"
   value       = aws_sns_topic.new_topic.name
@@ -11,27 +6,6 @@ output "topic_name" {
 output "topic_arn" {
   description = "ARN for the topic"
   value       = aws_sns_topic.new_topic.arn
-}
-
-output "access_key_id" {
-  description = "The access key ID"
-  value       = aws_iam_access_key.user.id
-}
-
-output "secret_access_key" {
-  description = "The secret access key ID"
-  value       = aws_iam_access_key.user.secret
-}
-
-output "additional_access_keys" {
-  description = "The list of access keys for additional teams"
-  value = {
-    for client in var.additional_topic_clients : client => {
-      user_name         = aws_iam_user.additional_users[client].name
-      access_key_id     = aws_iam_access_key.additional_users[client].id
-      secret_access_key = aws_iam_access_key.additional_users[client].secret
-    }
-  }
 }
 
 output "irsa_policy_arn" {
